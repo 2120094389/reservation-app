51e04e0f9172589ad16daf0dc95b7540b552985d
Tetsuya Nakamura <2120094389@campus.ouj.ac.jp>
2022-08-17 14:52:06 +0900

    Angularフロントエンド側作成

---
+++ src/app/common/test-path/test-path.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-path',
+  templateUrl: './test-path.component.html',
+  styleUrls: ['./test-path.component.scss']
+})
+export class TestPathComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
